293d1da94fc9fd8accecaaddfdb94d8cc999ea67
b2k8786 <b2k8786@gmail.com>
2020-12-10 13:26:05 +0530

    dummy feature test

---
+++ tests/Feature/FeatureTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+// use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class FeatureTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function check_features()
+    {
+        $this->assertTrue(true);
+    }
+}
